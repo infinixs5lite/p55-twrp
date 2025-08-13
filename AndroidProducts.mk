601b40bf7c895d645113cfcef54060016aa3a273
Kevin chinedu <kelvinchinedu97@gmail.com>
2025-08-13 05:59:39 +0100

    h

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_itel-P661N.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_itel-P661N-user \
+    omni_itel-P661N-userdebug \
+    omni_itel-P661N-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Itel itel P661N (itel-P661N)
+
+```
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
